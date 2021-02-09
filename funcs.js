678e61ed7e0f4348606bfbfd68021db2fb191425
Rick <rmansfield@realpeoplesratings.com>
2021-02-09 12:00:56 -0600

    first commit

---
+++ funcs.js
@@ -0,0 +1,81 @@
+let rangeNum = get_range("Enter max play range 1-N as a number:");
+//rangeNum
+function get_range(prompt){
+    let valid_input = false;
+    let range, input;
+    let instructions_message = document.getElementById("instructions");
+    
+    while(!valid_input) {
+        input = window.prompt(prompt);
+
+        range = Math.round(Number(input));
+        console.log(range);
+       
+       if (isNaN(range)) {
+            alert("please enter a valid number");
+        }
+        else if ( range == 0 || range == null){
+            alert("range can't be zero, null, or empty set. Enter valid numerical range");
+        }
+        else if ( range < 0) {
+            alert("Lets keep things positive.")
+        }
+        else {
+            valid_input = true;
+            instructions_message.innerHTML = "Remember to guess between 1 and " + range;
+            
+        }
+    }
+    return range;
+}
+
+
+//On Load Generate Random Number 1-N
+ //function generate_random() {
+  
+        let randnum = Math.floor(Math.random() * rangeNum) + 1;
+        console.log(randnum); //uncomment to test
+   // }
+
+    
+    
+    // collect user guess convert from string to number & validate
+function validate_guess() {
+    let guess = Number(document.getElementById("uguess").value);
+   
+    // assign message paragraph a usable variable
+    
+    let message = document.getElementById("results-messages");
+    //console.log(guess); //uncomment to test
+    
+    if(isNaN(guess)){
+        alert("That's not a number!");
+    } 
+    else if(guess > rangeNum || guess < 1) {
+        alert("That number is not in range, try again.");
+    }
+    else if(guess == randnum){
+        message.innerHTML = "You got it!";
+    } 
+    else if(guess > randnum) {
+        message.innerHTML = "No, try a lower number.";
+    }
+    else if ( guess < randnum) {
+        message.innerHTML ="No, try a higher number.";
+    }
+console.log(guess);
+}
+
+//initialize the array
+
+let guessed = [];
+//function pushData(){
+    //get values from user
+    let guess = Number(document.getElementById("uguess").value);
+    
+    //append array
+    guessed.push(guess);
+    console.log(guessed);
+//}
+
+
---
+++ highlow.js
@@ -0,0 +1,11 @@
+//Promt User for range and validate as numerical
+//let rangeNum = get_range("Enter max play range 1-N as a number:");
+
+//generate random number based on range above
+//let generateRandom = generate_random();
+
+//collect user guess and validate as numerical
+//let validatedGuess = validate_guess();
+
+//compare validated guess to random number
+//let guessVsRandom = compare();
